fa2e0b48bbd6bb29494560a7db273f602f7eaffe
ponperumalvp <ponperimal1202@gmail.com>
2024-01-27 09:08:35 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+
+function App() {
+  return (
+    <div className="bg-gradient-to-r from-orange-300 via-white  to-slate-400">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
